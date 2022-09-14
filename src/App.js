78d5e8b06fe649382151d927cec50b437cfe1d7a
Soham Mukherjee <sm2421@cse.jgec.ac.in>
2022-09-14 21:19:29 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import "./Styles.css";
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit, //Overwriting the result of previous calculation
+          overwrite: false,
+        };
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      //Basically above line of code prevents multiple 0's at the time in the beginning
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
+      //Above line of code prevents multiple "." in the current input(123.56.25 doesn't make sense but 123.56 makes)
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        // console.log("Both null executes");
+        return state;
+      }
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          //If u mistyped any operation and want to rewrite that, then this handler will come into effect
+        };
+      }
+      if (state.previousOperand == null) {
+        // console.log("This executes");
+        return {
+          //When previous operand is null,i.e, using at start time then prev is null and we enter the current operand and it's corresponding operation and then clicking on the operation button the current operand becomes null making space for the new current operand which will be needed to perform operation with the prev operand 20:40 timestamp
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+    case ACTIONS.CLEAR:
+      return {};
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state; //Basically meaning if we don't have appropiate credentials do nothing
+      }
+      return {
+        //This part evaluates the expression value, sets previous to null, operation to null and puts the evaluated value as currentOperand
+        ...state,
+        currentOperand: evaluate(state),
+        overwrite: true,
+        //After completing evaluation, if u write any value,the previous currentOperand should get overwritten as in normal calculator, so we set overwrite to true here and then we'll check in ADD_DIGIT if overwrite is true, then put a new value and set overwrite to false again(around 26:50)
+        previousOperand: null,
+        operation: null,
+      };
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: null,
+          overwrite: false,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    default:
+      return state;
+  }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const curr = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(curr)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + curr;
+      break;
+    case "-":
+      computation = prev - curr;
+      break;
+    case "*":
+      computation = prev * curr;
+      break;
+    case "÷":
+      computation = prev / curr;
+      break;
+  }
+  return computation.toString();
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+        className="span-two"
+      >
+        =
+      </button>
     </div>
   );
 }
